962d5607d984efcf1cd58271f47f97f36c498c70
Gulxumor <gulxumorerkinjonova2005@gmail.com>
2023-03-08 15:28:14 +0500

    Educational figma

---
+++ educational-figma/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Nav from './NavbarStyle';
+
+class Navbar extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+
+    render() {
+        return (
+            <Nav>
+                navbar
+            </Nav>
+        );
+    }
+
+}
+export default Navbar;
---
+++ educational-figma/src/Components/Navbar/NavbarStyle.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const Nav = styled.nav`
+display: flex;
+align-items: center;
+justify-content: space-between;
+width: 100%;
+border: 2px solid red;
+
+`;
+
+export default Nav;
