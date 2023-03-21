0021a042ca8f3d00e6e938b357284dbe6ffe7ae3
Gulxumor <gulxumorerkinjonova2005@gmail.com>
2023-03-21 09:42:05 +0500

    continuing

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './Components/App';
+import Root from './Root/Root';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Root />
   </React.StrictMode>
 );
