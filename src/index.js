a276467e25a781da2530057e113926adf5d6e944
Gulxumor <gulxumorerkinjonova2005@gmail.com>
2023-06-04 22:30:15 +0500

    cleaned

--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Navbar from './Components/Navbar/Navbar';
 import './index.css';
+import Root from './root';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <Navbar />
-  </React.StrictMode>
+  <Root />
 );
